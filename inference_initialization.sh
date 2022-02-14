3801f9a767514664d91c68879e5ee75f03a0c75c
pragasv <pragasv01@gmail.com>
2022-02-14 16:26:23 +0000

    Update code

--- inference_initialization.sh
+++ inference_initialization.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-python inference.py --checkpoint_path gh://ml-storage-dev/wav2lip_weights/wave2lip_gan.pth --face gh://ml-storage-dev/initail_inference_inputs/quasi_presenter_sample1_5s_sample1.mp4  --audio gh://ml-storage-dev/initail_inference_inputs/output.wav
+python inference.py --checkpoint_path wave2lip_gan.pth --face initail_inference_inputs/quasi_presenter_sample1_5s_sample1.mp4  --audio initail_inference_inputs/output.wav
